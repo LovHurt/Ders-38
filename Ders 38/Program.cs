0dd12a3c11b3c0f12444b7e7a8038e51b8039e4a
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-26 14:17:44 +0300

    Add project files.

---
+++ Ders 38/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.ID = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                ID = 2,
+                City = "İstanbul",
+                FirstName = "Derin",
+                LastName = "Demiroğ"
+            };
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine();
+        }
+    }
+}
